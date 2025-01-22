5171b86733a8f9ca8ae1b504bf7149c237a7a183
Gustavo Caixeta <gustavohcaixeta@gmail.com>
2025-01-22 07:49:00 -0300

    feat: first commit

---
+++ src/test/java/tech_cx/adi/AdiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech_cx.adi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
