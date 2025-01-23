c7040c7e9199606cbbeb4cc1566c051662d83f4c
Gustavo Caixeta <gustavohcaixeta@gmail.com>
2025-01-23 08:03:04 -0300

    feat: create entity `User`

---
+++ src/main/java/tech_cx/adi/entity/User.java
@@ -0,0 +1,47 @@
+package tech_cx.adi.entity;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID userId;
+
+  @Column(name = "username")
+  private String username;
+
+  @Column(name = "email")
+  private String email;
+
+  @Column(name = "password")
+  private String password;
+
+  @CreationTimestamp
+  private Instant creationTimestamp;
+
+  @UpdateTimestamp
+  private Instant updateTimestamp;
+
+  
+
+}
