f0490751dbca3d7281c6bebca35d21a250e441c2
Gustavo Caixeta <gustavohcaixeta@gmail.com>
2025-01-27 13:29:04 -0300

    refactor: add annotation AllArgsConstructor in `User`

--- src/main/java/tech_cx/adi/entity/User.java
+++ src/main/java/tech_cx/adi/entity/User.java
@@ -12,6 +12,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,6 +22,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
   @Id
