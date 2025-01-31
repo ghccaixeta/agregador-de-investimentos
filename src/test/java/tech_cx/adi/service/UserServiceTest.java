031ca43373cf52cda4a33e352f647a77a30e687c
Gustavo Caixeta <gustavohcaixeta@gmail.com>
2025-01-31 08:13:22 -0300

    feat: create test for `createUser`

---
+++ src/test/java/tech_cx/adi/service/UserServiceTest.java
@@ -0,0 +1,67 @@
+package tech_cx.adi.service;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import tech_cx.adi.controller.CreateUserDto;
+import tech_cx.adi.entity.User;
+import tech_cx.adi.repository.UserRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+  //Create a mock of the UserRepository
+  @Mock
+  private UserRepository userRepository;
+
+  // Inject the mock into the UserService
+  @InjectMocks
+  private UserService userService;
+  
+  @Nested
+  class createUser {
+    @Test
+    @DisplayName("Should create a user with success")
+    void shouldCreateAUser() {
+      //Arrange
+
+      var user = new User(
+        UUID.randomUUID(),
+        "lsccaixeta", 
+        "luisa@gmail.com", 
+        "1234567",
+        Instant.now(),
+        null
+      );
+
+      // Retun the user when save is called
+      doReturn(user).when(userRepository).save(any());
+
+      var input = new CreateUserDto(
+        "lsccaixeta", 
+        "luisa@gmail.com", 
+        "1234567"
+      );
+  
+      //Act
+      var output = userService.createUser(input);
+  
+      //Assert
+      assertNotNull(output);
+    }
+
+  }
+
+}
