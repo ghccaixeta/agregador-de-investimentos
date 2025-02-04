ba84b471f65d8eafebd28100fd2a43101863dad1
Gustavo Caixeta <gustavohcaixeta@gmail.com>
2025-02-04 07:38:46 -0300

    feat: create test to validate `Throw Exception`

--- src/test/java/tech_cx/adi/service/UserServiceTest.java
+++ src/test/java/tech_cx/adi/service/UserServiceTest.java
@@ -1,8 +1,10 @@
 package tech_cx.adi.service;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -60,6 +62,33 @@ public class UserServiceTest {
   
       //Assert
       assertNotNull(output);
+    }
+
+    @Test
+    @DisplayName("Should throw exception when error occurs")
+    void shouldThrowExceptionWhenErrorOccurs() {
+      
+      //Arrange
+
+      var user = new User(
+        UUID.randomUUID(),
+        "lsccaixeta", 
+        "luisa@gmail.com", 
+        "1234567",
+        Instant.now(),
+        null
+      );
+
+      // Retun the user when save is called
+      doThrow(new RuntimeException()).when(userRepository).save(any());
+
+      var input = new CreateUserDto(
+        "lsccaixeta", 
+        "luisa@gmail.com", 
+        "1234567"
+      );
+  
+      assertThrows(RuntimeException.class, () -> userService.createUser(input));
     }
 
   }
